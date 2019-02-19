c487146f9b36c1677e51ab9ca2ad4761afc30b4a
za-chenzhenyuan <jason@chenzhenyuan.com>
2019-02-19 20:29:30 +0800

    init: Create the projectt

---
+++ src/index.js
@@ -0,0 +1,53 @@
+/* eslint-disable */
+
+const { Command, Option } = require('commander');
+
+function pad (str, width) {
+    const len = Math.max(0, width - str.length);
+
+    return str + Array(len + 1).join(' ');
+}
+
+
+Command.prototype.version = function (str, flags) {
+    if (arguments.length === 0) return this._version;
+    this._version = str;
+    flags = flags || '-v, --version';
+    var versionOption = new Option(flags, '输出版本号');
+    this._versionOptionName = versionOption.long.substr(2) || 'version';
+    this.options.push(versionOption);
+    this.on('option:' + this._versionOptionName, function() {
+      process.stdout.write(str + '\n');
+      process.exit(0);
+    });
+    return this;
+};
+
+
+Command.prototype.optionHelp = function () {
+    const width = this.padWidth();
+
+    const options = this.options.map((option) => {
+      const flag  = pad(option.flags, width);
+      const desc  = option.description;
+      const value = option.bool && option.defaultValue !== undefined ? ` (default: ${JSON.stringify(option.defaultValue)})` : '';
+
+      return `${flag}    ${desc}${value}`
+    });
+
+    return options.concat([ `${pad('-h, --help', width)}    ` + '输出使用帮助' ]).join('\n');
+};
+
+
+Command.prototype.outputHelp = function(caller) {
+  const callback = caller ? caller : (passthru) => passthru;
+  const source = callback(this.helpInformation()).split('\n');
+  const format = source.map((str)=>`  ${str}`)
+
+  process.stdout.write(`\n${format.join('\n')}\n`);
+
+  this.emit('--help');
+};
+
+
+module.exports = new Command();
